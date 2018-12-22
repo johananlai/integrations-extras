74806ed84e1cd9cee7652db2f6fbcb5922f05d48
johananlai <lai.johanan@gmail.com>
2018-12-21 16:10:57 -0800

    [twitch] Collect first POC test metric

--- twitch/datadog_checks/twitch/twitch.py
+++ twitch/datadog_checks/twitch/twitch.py
@@ -1,4 +1,6 @@
 import requests
+import simplejson as json
+from six.moves.urllib.parse import urljoin
 
 from datadog_checks.checks import AgentCheck
 
@@ -19,3 +21,31 @@ class TwitchCheck(AgentCheck):
             raise Exception("Missing 'client_id' in Twitch config")
 
         client_id = instance['client_id']
+
+        payload = {}
+        try:
+            payload = self._get_metrics(instance, api_url, client_id)
+        except Exception, e:
+            self.log.error("Failed to get metrics with error: {}".format(e))
+
+        try:
+            self._report_metrics(instance, payload)
+        except Exception, e:
+            self.log.error("Failed to report metrics with error: {}".format(e))
+
+    def _report_metrics(self, instance, payload):
+        metric_name = 'twitch.viewer.count'
+        self.count(metric_name, payload['data'][0]['viewer_count'],
+                   tags=instance.get('tags', []) + ['channel:' + payload['data'][0]['user_name']])
+
+    def _get_metrics(self, instance, api_url, client_id):
+        # TEMP
+        path = "streams"
+
+        headers = {'Client-ID': client_id}
+        params = {'first': '1'}
+        r = requests.get(urljoin(api_url, path), headers=headers, timeout=60)
+        r.raise_for_status()
+
+        return json.loads(r.text)
+
