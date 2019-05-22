e1108532a5bf725792b00de7b63a49f26da8b49c
johananlai <lai.johanan@gmail.com>
2019-05-22 10:17:51 -0700

    Remove old twitchtv integration files

--- twitch/datadog_checks/twitch/__init__.py
+++
@@ -1,7 +0,0 @@
-from .__about__ import __version__
-from .twitch import TwitchCheck
-
-__all__ = [
-    '__version__',
-    'TwitchCheck'
-]
--- twitch/tests/test_twitch.py
+++
@@ -1,8 +0,0 @@
-from datadog_checks.twitch import TwitchCheck
-
-
-def test_check(aggregator, instance):
-    check = TwitchCheck('twitch', {}, {})
-    check.check(instance)
-
-    aggregator.assert_all_metrics_covered()
